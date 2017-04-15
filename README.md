5b0d792820530b5c16900b9770639cdaefd0d621
薫 太田 <hardrock.is.my.life@gmail.com>
2017-04-15 22:03:30 +0900

    ソリューションのみ作成。内容は意味なし。

---
+++ README.md
@@ -0,0 +1,1 @@
+# NotAmplifier-AVR-CS" >> README.md
---
+++ sources/avr/NotAmplifier/main.c
@@ -0,0 +1,18 @@
+/*
+ * NotAmplifier.c
+ *
+ * Created: 2017/04/15 21:53:36
+ * Author : Jackhammer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
